9fcf918b11bd62313ab20ecb633c9593be897374
Chase Chaser <thunte11@vols.utk.edu>
2021-08-21 16:51:22 -0400

    Placerholders

---
+++ Instance/avocat.cpp
@@ -0,0 +1,17 @@
+/*
+    Avocat instances.
+        Called by user in the command line to use the
+        program.
+*/
+
+#include <iostream>
+using namespace std;
+
+//  Placeholder //
+int main()
+{
+    printf("Hello world!\n");
+    return 1;
+}
+
+
