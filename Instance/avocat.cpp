a4e852eafdf4fa230acdb12869e7dc8238464014
Chase Chaser <thunte11@vols.utk.edu>
2021-08-21 17:23:35 -0400

    Almost done with #3

--- Instance/avocat.cpp
+++ Instance/avocat.cpp
@@ -5,12 +5,37 @@
 */
 
 #include <iostream>
+#include <string>
 using namespace std;
 
 //  Placeholder //
-int main()
+int main(int argc, char *argv[])
 {
-    printf("Hello world!\n");
+
+    string combinedArgs;
+
+    //  Check argc  //
+    if(argc <= 1)
+    {
+        cerr << "Not enough argumnts.\n";
+        return 1;
+    }
+
+    //  Assigns first string    //
+    combinedArgs = argv[1];
+
+    //  Loop through string //
+    for(int i = argc-1; i > 1; i--)
+    {
+        combinedArgs += ' ';
+        combinedArgs +=  argv[i];
+    }
+
+    cout << combinedArgs << '\n';
+
+    //  Use system comand   //
+    //system(combinedArgs);
+
     return 1;
 }
 
