3567dbc97ff654c62cb24328c323214e5a679c0a
Gregory Croisdale <gcroisda@vols.utk.edu>
2021-08-26 21:49:56 -0400

    some housekeeping, basic server set up, slight existential crisis

--- avocat-local/main.cpp
+++ avocat-local/main.cpp
@@ -1,3 +1,17 @@
+/*
+ *  avocat-local
+ *
+ *  This is a daemon which mediates and sometimes interjects between
+ *  user-level avocat inquiries and a server.
+ * 
+ *  Author: Gregory Croisdale
+ *  University of Tennessee, Knoxville
+ * 
+ */
+
+// DEFINITE CHANGE OF HEART
+// this probably doesn't need to exist
+
 #include <iostream>
 #include <string>
 
@@ -5,21 +19,47 @@
 #include <sys/socket.h>
 #include <stdlib.h>
 #include <netinet/in.h>
+#include <string.h>
+
+#include "avocat.hpp"
 
 int main(int argc, char** argv) {
+    /* Create server and bind to port */
     int server_fd;
-    struct sockaddr local_addr;
-    char buffer[256];
+    struct sockaddr_in local_addr;
+    char buff[257];
+    uint16_t port;
 
-    if (!(server_fd = socket(AF_INET, SOCK_STREAM, 0))) {
+    bzero(&local_addr, sizeof(local_addr));
+
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
         perror("Socket creation error");
         exit(EXIT_FAILURE);
     }
 
-    if (bind(server_fd, &local_addr, INADDR_ANY)) {
-        fprintf(stderr, "Failed to bind port %d: ", *((unsigned short*) (((char*) &local_addr) + sizeof(short))));
+    local_addr.sin_family = AF_INET;
+    local_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    local_addr.sin_port = htons(INADDR_ANY);
+
+    if (bind(server_fd, (struct sockaddr*) &local_addr, sizeof(local_addr))) {
+        fprintf(stderr, "Failed to bind port %d: ", (int) local_addr.sin_port);
         perror("");
         exit(EXIT_FAILURE);
+    }
+
+    socklen_t len = sizeof(local_addr);
+    if (getsockname(server_fd, (struct sockaddr *)&local_addr, &len)) {
+        perror("getsockname");
+    }
+
+    printf("port number %d\n", ntohs(local_addr.sin_port));
+
+    /* Create description file for daemon */
+
+    /* Closing time */
+    if (close(server_fd)) {
+        fprintf(stderr, "did not close!\n");
+        perror("");
     }
 
     // int r = daemon(0, 0);
